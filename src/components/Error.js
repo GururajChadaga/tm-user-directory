c39df62702ddbc78ef52638ed95f634783921cea
Gururaj Chadaga <guruuraj.chadaga@gmail.com>
2023-12-17 20:20:00 +0530

    feat: add error component

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import styles from './Error.module.css';
+
+const Error = () => {
+  return (
+    <div className={styles.wrapper}>
+      <span>Something went wrong!</span>
+    </div>
+  );
+};
+
+export default Error;
